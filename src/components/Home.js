52532d062a4c905415777107a6dc51fd435b7aaf
Water Palma <programerbird@gmail.com>
2023-10-31 19:55:03 -0700

    new_header_search

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,39 @@
 import React from 'react'
 import './Home.css'
-import iconsvg from "../Pictures/logo.png";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import pfp from '../Pictures/pfp.jpg'
+
 const Home = () => {
   return (
     <div className='main_wrapper' >
-      <div className='header'>
-        <img src={iconsvg} alt='logo'/>
-          <div className='links' >
-            <a href='link' >Chat</a>
-            <a href='link' >Anime</a>
-            <a href='link' >Manga</a>
-            <a href='link' >setting</a>
-              <div>
-              <Box
-                component="form"
-                sx={{
-                '& > :not(style)': { m: -2, width: '10ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                  <TextField id="filled-basic" label="Search" variant="filled" />
-              </Box>
-              </div>
+      <div className='main_text' >
+        <h1>ANIMEU</h1>
+          <a style={{color:'yellow'}} href='link'>CHAT</a>
+          <a style={{color:'yellow'}} href='link'>ANIME</a>
+          <a style={{color:'yellow'}} href='link'>MANGA</a>
+          <Stack direction="row" spacing={2}>
+              <Avatar alt="Remy Sharp" src={pfp} />
+           </Stack>
+          <div>
+          <Box
+            component="form"
+             sx={{
+            '& > :not(style)': { m: 1, width: '30ch' },
+            }}
+             noValidate
+             autoComplete="off"
+            >
+            <TextField id="outlined-basic" label="Search..." variant="outlined" />
+          </Box>
+           
           </div>
-          
       </div>
+       
+    
+     
     </div>
   )
 }
