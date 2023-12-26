81522a42837914425dbf164bb2c6f6113505da80
Water Palma <programerbird@gmail.com>
2023-12-26 14:40:44 -0800

    save

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,9 @@ const Home = () => {
           <a style={{textDecoration: "none"}}  href='link'>MANGA</a>
           <a style={{textDecoration: "none"}}  href='link'>MANGA</a>
           <a style={{textDecoration: "none"}}  href='link'>MANGA</a>
+          <a style={{textDecoration: "none"}}  href='link'>MANGA</a>
+          <a style={{textDecoration: "none"}}  href='link'>MANGA</a>
+          <a style={{textDecoration: "none"}}  href='link'>MANGA</a>
           
           
           </div>
