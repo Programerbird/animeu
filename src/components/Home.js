3bac631407d4fba41da4577c9712faeb76a92936
Programerbird <programerbird@gmail.com>
2024-03-13 16:10:20 -0700

    realtime

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,6 +23,10 @@ const Home = () => {
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
            
           </Stack>
           
