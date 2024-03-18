e7f891e4df20d295ce6b7a3c7a6d848515912036
Programerbird <programerbird@gmail.com>
2024-03-17 20:58:34 -0700

    looksufas

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,6 +32,7 @@ const Home = () => {
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
            
           </Stack>
           
