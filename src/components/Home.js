d475ea8cd924622b037274b6852a9ed884e46548
Programerbird <programerbird@gmail.com>
2024-04-04 20:07:33 -0700

    appleow

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,6 +25,9 @@ const Home = () => {
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Manga</Button>
             <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Manga</Button>
+            <Button variant="contained">Chat</Button>
           </Stack>
           
           <Stack  direction="row" spacing={2}>
