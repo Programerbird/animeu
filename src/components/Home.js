e71aa707bf3f7aef32ec2b791454491286da1f27
Programerbird <programerbird@gmail.com>
2024-04-24 21:32:24 -0700

    curseit

--- src/components/Home.js
+++ src/components/Home.js
@@ -44,6 +44,9 @@ const Home = () => {
             <Button variant="contained">Anime</Button>
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Anime</Button>
+            <Button variant="contained">Anime</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Anime</Button>
           </Stack>
           
           <Stack  direction="row" spacing={2}>
