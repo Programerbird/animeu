f9204f3bd9c0383398589228c7b02f7d0164b2bb
Programerbird <programerbird@gmail.com>
2024-05-11 22:49:25 -0700

    your

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,6 +26,8 @@ const Home = () => {
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Anime</Button>
             <Button variant="contained">Chat</Button>
+            <Button variant="contained">Anime</Button>
+            <Button variant="contained">Chat</Button>
           </Stack>
           
           <Stack  direction="row" spacing={2}>
