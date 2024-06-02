b3ad9f211fe9341a590cd9148caafecedd2e892e
Programerbird <programerbird@gmail.com>
2024-06-01 17:43:53 -0700

    ps4

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,6 +68,13 @@ const Home = () => {
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Chat</Button>
             <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
+            <Button variant="contained">Chat</Button>
           </Stack>
           
           <Stack  direction="row" spacing={2}>
